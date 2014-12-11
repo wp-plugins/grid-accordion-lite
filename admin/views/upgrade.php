5084bc60bbb5704e8d933d92b184703c33bc92ff
bqworks <bqworks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-11 16:52:36 +0000

    some bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/grid-accordion-lite/tags/1.1.1@1042800 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/views/upgrade.php
@@ -0,0 +1,25 @@
+<div class="wrap grid-accordion-admin grid-accordion-upgrade">
+	<h2><?php _e( 'Upgrade to Full Version', 'grid-accordion-lite' ); ?></h2>
+    <p>If you need more features or simply want to support the development of the Grid Accordion plugin, you can upgrade to the full version.</p>
+    
+    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $18 &rarr;</a></p>
+    
+    <p>Some of the available features are:</p>
+    <ul>
+        <li>Animated and static layers, which can contain text, images or any HTML content</li>
+        <li>Load images and content automatically, from posts, galleries and Flickr</li>
+        <li>Break the grid accordion into multiple pages</li>
+        <li>Retina support</li>
+        <li>Lazy loading</li>
+        <li>Deep linking</li>
+        <li>Lightbox integration</li>
+        <li>Swap image when the panel is opened</li>
+        <li>Breakpoints for changing the grid accordion's settings at different screen sizes</li>
+        <li>Action and filter hooks</li>
+    </ul>
+    
+    <p>You can see some examples and read more about the available features at <a href="http://bqworks.com/grid-accordion/">http://bqworks.com/grid-accordion/</a>.</p>
+    <p>Also, you can check out the <a href="https://www.youtube.com/watch?v=XqeHLv052Bc&list=PLh-6IaZNuPo7Skwefhb9T2CSazDjC56Lg">video tutorials</a> to understand better how the plugin works.</p>
+    
+    <p><a href="http://codecanyon.net/item/grid-accordion-responsive-wordpress-plugin/7788530?ref=bqworks">Buy the full version for only $18 &rarr;</a></p>
+</div>
---
+++ grid-accordion-lite.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+	Plugin Name: Grid Accordion Lite
+	Plugin URI:  http://bqworks.net/grid-accordion/
+	Description: Responsive and touch-enabled grid accordion. The lite version.
+	Version:     1.1.1
+	Author:      bqworks
+	Author URI:  http://bqworks.com
+*/
+
+// if the file is called directly, abort
+if ( ! defined( 'WPINC' ) ) {
+	die();
+}
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-grid-accordion.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-accordion-renderer.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-panel-renderer-factory.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-panel-renderer.php' );
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-grid-accordion-activation.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-grid-accordion-widget.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-grid-accordion-settings.php' );
+
+register_activation_hook( __FILE__, array( 'BQW_Grid_Accordion_Lite_Activation', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'BQW_Grid_Accordion_Lite_Activation', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'BQW_Grid_Accordion_Lite', 'get_instance' ) );
+add_action( 'plugins_loaded', array( 'BQW_Grid_Accordion_Lite_Activation', 'get_instance' ) );
+
+// register the widget
+add_action( 'widgets_init', 'bqw_gal_register_widget' );
+
+if ( is_admin() ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-grid-accordion-admin.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-grid-accordion-updates.php' );
+	add_action( 'plugins_loaded', array( 'BQW_Grid_Accordion_Lite_Admin', 'get_instance' ) );
+	add_action( 'admin_init', array( 'BQW_Grid_Accordion_Lite_Updates', 'get_instance' ) );
+}
